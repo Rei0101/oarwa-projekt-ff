a837c87029f6d22083615dae3fa67b3c06f08598
Rei0101 <62380465+Rei0101@users.noreply.github.com>
2025-03-07 18:16:01 +0100

    Apply .env variable to host name

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,15 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'client'
+  root: 'client',
+  server: {
+    port: process.env.CLIENT_PORT || 5173
+  },
 })

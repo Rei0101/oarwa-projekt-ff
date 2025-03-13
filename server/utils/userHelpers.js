4451bacee3ac3b8fe2d969c02f678e9f64074d2a
Rei0101 <62380465+Rei0101@users.noreply.github.com>
2025-03-13 20:03:51 +0100

    Add user helper file (for checking duplicated items in db)

---
+++ server/utils/userHelpers.js
@@ -0,0 +1,9 @@
+async function checkFieldAppearance(conditions, User) {
+    for (const key in conditions) {
+        if (await User.findOne({ [key]: conditions[key] })) {
+            throw Object.assign(new Error(), { status: 409 });
+        }
+    }
+}
+
+export {checkFieldAppearance};

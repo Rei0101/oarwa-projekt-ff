c03f83c16672e7f743e9a5b52aad4eb746f68d2f
Rei0101 <62380465+Rei0101@users.noreply.github.com>
2025-04-06 21:08:51 +0200

    Implement base frontend menu page design

---
+++ client/src/components/MenuItem.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+export default function MenuItem({ imageLink, name, ingredients, price }) {
+  return (
+    <div className="item">
+      <img src={imageLink} alt={name + " slika"} />
+      <div>
+        <span>
+            <h3>{name}</h3>
+            <p>{ingredients.join(", ")}</p>
+        </span>
+        <span>
+            <p>{price + " €"}</p>
+            <button>Naruči!</button>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+MenuItem.propTypes = {
+  imageLink: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  ingredients: PropTypes.array.isRequired,
+  price: PropTypes.number.isRequired,
+};
--- client/src/pages/Menu/Menu.jsx
+++ client/src/pages/Menu/Menu.jsx
@@ -1,9 +1,30 @@
 import "./Menu.css";
+import MenuItem from "../../components/MenuItem";
 
 function Menu() {
   return (
     <div className="container">
-      Meni
+      <input placeholder="Filter" type="text" />
+      <div id="menu">
+        <MenuItem
+          imageLink="https://th.bing.com/th/id/OIP.RWaaZCnAJwE8O0FxwD7y0AHaEo?rs=1&pid=ImgDetMain"
+          name="Miješana pizza"
+          ingredients={["umak od pome", "šunka",]}
+          price={12.99}
+        />
+        <MenuItem
+          imageLink="https://th.bing.com/th/id/OIP.RWaaZCnAJwE8O0FxwD7y0AHaEo?rs=1&pid=ImgDetMain"
+          name="Miješana pizza"
+          ingredients={["umak od pome", "šunka",]}
+          price={12.99}
+        />
+        <MenuItem
+          imageLink="https://th.bing.com/th/id/OIP.RWaaZCnAJwE8O0FxwD7y0AHaEo?rs=1&pid=ImgDetMain"
+          name="Miješana pizza"
+          ingredients={["umak od pome", "šunka",]}
+          price={12.99}
+        />
+      </div>
     </div>
   );
 }

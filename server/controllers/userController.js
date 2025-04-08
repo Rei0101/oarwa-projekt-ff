727cfd0bfcab7b06eead18e86f76d0affa8c0513
Rei0101 <62380465+Rei0101@users.noreply.github.com>
2025-04-09 00:12:16 +0200

    Implement various controller registerUser features
    Implemented existing password validation function for backend to given password.
    Implement different user roles (user/admin) based on email domain.
    Added (previously missing) function for handling collections.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,17 +1,30 @@
 import CONFIG from "../config/config.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import CustomError from "../../shared/CustomErrorClass.js"
+import CustomError from "../../shared/CustomErrorClass.js";
 import { User } from "../models/userModel.js";
+import { validPassword } from "../utils/validation.js";
+import { handleUserRole } from "../utils/handlers.js";
 import { checkFieldAppearance } from "../utils/helpers.js";
 
 const registerUser = async (req, res, next) => {
-  const { email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const newUser = new User({ ...req.body, password: hashedPassword });
-
   try {
+    const { email, password } = req.body;
+
+    if (!validPassword(password)) {
+      return next(
+        new CustomError(400, `Password: ${password} is not a valid password.`)
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      ...req.body,
+      password: hashedPassword,
+      role: handleUserRole(email),
+    });
+
     await checkFieldAppearance({ email }, User);
     await newUser.save();
     res.status(201).send(newUser);

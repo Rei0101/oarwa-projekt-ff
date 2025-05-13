243ba53822244ebd076dee1ef33077a2e5d6dc95
Rei0101 <62380465+Rei0101@users.noreply.github.com>
2025-05-13 19:25:39 +0200

    Update duplicationCheckingMiddleware.js to exclude uniqueness when updating

--- server/middleware/duplicationCheckingMiddleware.js
+++ server/middleware/duplicationCheckingMiddleware.js
@@ -3,11 +3,20 @@ import CustomError from "../../shared/CustomErrorClass.js";
 const duplicationCheckingMiddleware =
   (fields, Model) => async (req, res, next) => {
     try {
+      const idToExclude = req.params.id;
+
       for (const field of fields) {
         const value = req[field] ?? req?.body[field];
-        
-        if (await Model.findOne({ [field]: value })) {
-          next(new CustomError(409));
+        if (value === undefined) continue;
+
+        const query = { [field]: value };
+
+        if (idToExclude) {
+          query._id = { $ne: idToExclude };
+        }
+
+        if (await Model.findOne(query)) {
+          return next(new CustomError(409));
         }
       }
       next();

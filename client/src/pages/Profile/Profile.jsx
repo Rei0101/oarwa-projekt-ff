7d2355f775966f442ce20a8831b4f02694cfd32f
Rei0101 <62380465+Rei0101@users.noreply.github.com>
2025-05-28 12:02:05 +0200

    Fix app styling

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -26,46 +26,48 @@ function Profile() {
   return (
     <div className="container">
       {!error ? (
-        <>
-          <table >
-            <tr>
-              <th>Ime:</th>
-              <td>{fetched.firstName}</td>
-            </tr>
-            <tr>
-              <th>Prezime:</th>
-              <td>{fetched.lastName}</td>
-            </tr>
-            <tr>
-              <th>E-mail:</th>
-              <td>{fetched.email}</td>
-            </tr>
-            <tr>
-              <th>Lozinka:</th>
-              <td>●●●●●●●●●</td>
-            </tr>
-            <tr>
-              <th>Datum rođenja:</th>
-              <td>{fetched.dateOfBirth.slice(0, 10)}</td>
-            </tr>
-            <tr>
-              <th>Grad:</th>
-              <td>{fetched.city}</td>
-            </tr>
-            <tr>
-              <th>Adresa:</th>
-              <td>{fetched.address}</td>
-            </tr>
-            <tr>
-              <th>Broj mobitela:</th>
-              <td>{fetched.phone}</td>
-            </tr>
+        <div id="profile">
+          <table>
+            <tbody>
+              <tr>
+                <th>Ime:</th>
+                <td>{fetched.firstName}</td>
+              </tr>
+              <tr>
+                <th>Prezime:</th>
+                <td>{fetched.lastName}</td>
+              </tr>
+              <tr>
+                <th>E-mail:</th>
+                <td>{fetched.email}</td>
+              </tr>
+              <tr>
+                <th>Lozinka:</th>
+                <td>●●●●●●●●●</td>
+              </tr>
+              <tr>
+                <th>Datum rođenja:</th>
+                <td>{fetched.dateOfBirth.slice(0, 10)}</td>
+              </tr>
+              <tr>
+                <th>Grad:</th>
+                <td>{fetched.city}</td>
+              </tr>
+              <tr>
+                <th>Adresa:</th>
+                <td>{fetched.address}</td>
+              </tr>
+              <tr>
+                <th>Broj mobitela:</th>
+                <td>{fetched.phone}</td>
+              </tr>
+            </tbody>
           </table>
           <div>
             <button>Promijeni lozinku</button>
             <button>Odjavi se</button>
           </div>
-        </>
+        </div>
       ) : (
         <h3>{error}</h3>
       )}
